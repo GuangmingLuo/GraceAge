7a8fc8d931839d73b4fcdbd3b1f0d919d8f51cb0
r0481919 <r0481919@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-07 16:57:00 +0000

    Registration Page: Added two functions in AccountController.php; Created a new view page register.html. Now user can sign up and log in with the account he/she created.

--- GraceAge/application/controllers/AccountController.php
+++ GraceAge/application/controllers/AccountController.php
@@ -28,4 +28,30 @@ class AccountController extends CI_Controller {
              }
         $this->parser->parse('Account/login.html',$data);
     }
+    
+    public function register() {
+        $data['register_state'] = 'Ready to log in';
+        $this->parser->parse('Account/register.html', $data);
+    }
+
+    public function registerPost() {
+        $usertype = filter_input(INPUT_POST, 'usertype');        
+        $username = filter_input(INPUT_POST, 'username');
+        $password1 = filter_input(INPUT_POST, 'password1');
+        $password2 = filter_input(INPUT_POST, 'password2');
+        $this->load->database();
+        $query = $this->db->query("SELECT Name FROM Patient where Name=?", $username);
+        $row = $query->row();
+        if (isset($row)) {
+            $data['register_state'] = 'This user has been registered';
+        }elseif($password1 === $password2){
+            $password = password_hash($password1, PASSWORD_DEFAULT);
+            $this->db->query("INSERT INTO $usertype (Name, password) VALUES ('$username','$password')" );
+            $data['register_state'] = 'Registration succeeds!';
+        }else{
+            $data['register_state'] = 'The passwords are not the same!';
+        }
+        $this->parser->parse('Account/register.html', $data);
+    }
+
 }
