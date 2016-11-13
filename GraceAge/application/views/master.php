de64513f9d596eb1d9233751aed4583dcc3b2553
r0647865 <r0647865@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-13 22:44:30 +0000

    use w3.css as internal file instead of external http, because chrome won't load files not coming from https

--- GraceAge/application/views/master.php
+++ GraceAge/application/views/master.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">        
         <link href="../../assets/css/login.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+        <link href="../../assets/css/w3.css" rel="stylesheet" type="text/css"/>
         <title>{page_title}</title>
     </head>
     <body>
