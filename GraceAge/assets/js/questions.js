21d2db86df22c9b896e1fc988774405f51b90b65
r0647865 <r0647865@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-15 16:31:06 +0000

    changed browser check condition and removed the reference to the .map in the jquery.min

--- GraceAge/assets/js/questions.js
+++ GraceAge/assets/js/questions.js
@@ -5,43 +5,44 @@
  */
 
 function determineBrowserForQR() {
-    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-    if (isSafari) {
-        document.getElementById('reader').style.display = 'none';
-    }
-    else {
-        document.getElementById('safariQRfunctionaity').style.display = 'none';
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.indexOf('safari') !== -1) {
+        if (ua.indexOf('chrome') > -1) {
+            document.getElementById('safariQRfunctionaity').style.display = 'none'; // Chrome
+        } else {
+            document.getElementById('reader').style.display = 'none'; // Safari
+        }
     }
 }
 
-$(function(){
-determineBrowserForQR();
+$(function () {
+    determineBrowserForQR();
 });
 
-$('.answer_button').click(function(){
+$('.answer_button').click(function () {
     //alert("Dit gebeurt wel...");
     $('.answer_button').css('background-color', "Lime");
     $(this).css('background-color', "White");
-    
+
 });
 
 
-function previous(){
-    $.getJSON("previous", function(data) {
-       $('.answer_button').css('background-color', "Lime");
-       $('#question_placeholder').text(data[0].Question);
-       $('#topic_placeholder').text(data[0].Topic);
-       //alert('something came in');
+function previous() {
+    $.getJSON("previous", function (data) {
+        $('.answer_button').css('background-color', "Lime");
+        $('#question_placeholder').text(data[0].Question);
+        $('#topic_placeholder').text(data[0].Topic);
+        //alert('something came in');
     });
 }
 
-function next(){
+function next() {
     //alert('hmmm');
     $('.answer_button').css('background-color', "Lime");
-    $.getJSON("next", function(data) {
-       $('#question_placeholder').text(data[0].Question);
-       $('#topic_placeholder').text(data[0].Topic);
-       //alert('something came in');
+    $.getJSON("next", function (data) {
+        $('#question_placeholder').text(data[0].Question);
+        $('#topic_placeholder').text(data[0].Topic);
+        //alert('something came in');
     });
 }
 
