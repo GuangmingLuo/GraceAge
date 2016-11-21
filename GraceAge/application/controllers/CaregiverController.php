56f2f083a133ae7d1a459a41b3435755f9c2ff32
r0364529 <r0364529@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-21 11:38:03 +0000

    Bereken score per topic in %

--- GraceAge/application/controllers/CaregiverController.php
+++ GraceAge/application/controllers/CaregiverController.php
@@ -11,7 +11,8 @@
  *
  * @author orditech
  */
-class CaregiverController extends CI_Controller{
+class CaregiverController extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
         $this->load->database();
@@ -22,8 +23,8 @@ class CaregiverController extends CI_Controller{
         $this->load->model('Caregiver_Menu_model');
         $this->load->model('Caregiver_Home_model');
     }
-    
-    function index(){
+
+    function index() {
         if ($this->session->userType == "Caregiver") { // if session exists
             $data['show_navbar'] = true;
             $data['page_title'] = 'Caregiver Home';
@@ -34,28 +35,43 @@ class CaregiverController extends CI_Controller{
             $data['urgent'] = $this->Caregiver_Home_model->get_patients();
             //$data['content'] = "This is the home page!";
             $data['content'] = "";
-            $data['page_content']='Caregiver/index.html';
-            $this->parser->parse('master.php',$data);
-        }else {
+            $data['page_content'] = 'Caregiver/index.html';
+            $this->parser->parse('master.php', $data);
+        } else {
             echo "You are not allowed to access this page!!!";
         }
     }
-    
-    function personal(){
+
+    function personal() {
         if ($this->session->userType == "Caregiver") { // if session exists
             $data['show_navbar'] = true;
             $data['page_title'] = 'Personal Patient Information';
             $data['caregiver_menu_items'] = $this->Caregiver_Menu_model->get_menuitems('Persoonlijk');
             $data['navbar_content'] = 'Caregiver/caregiverNavbar.html';
             $data['content'] = lang('hello'); //to check whether internationalization set up works
-            $data['page_content'] = 'Caregiver/template.html';
-            $this->parser->parse('master.php',$data);
-        }else {
+            $data['patients'] = $this->Caregiver_Home_model->get_patients();
+            $data['topics'] = $this->Caregiver_Home_model->get_topics();
+            $data['answerss'] = $this->Caregiver_Home_model->print_score('axel');
+            $data['privacy'] = $this->Caregiver_Home_model->calculate_topic('axel','Privacy');
+            $data['food'] = $this->Caregiver_Home_model->calculate_topic('axel','Food');
+            $data['safety'] = $this->Caregiver_Home_model->calculate_topic('axel','Safety');
+            $data['comfort'] = $this->Caregiver_Home_model->calculate_topic('axel','Comfort');
+            $data['autonomie'] = $this->Caregiver_Home_model->calculate_topic('axel','Autonomy');
+            $data['respect'] = $this->Caregiver_Home_model->calculate_topic('axel','Respect');
+            $data['staffresp'] = $this->Caregiver_Home_model->calculate_topic('axel','StaffResponse');
+            $data['staffbonding'] = $this->Caregiver_Home_model->calculate_topic('axel','StaffBonding');
+            $data['activities'] = $this->Caregiver_Home_model->calculate_topic('axel','Activities');
+            $data['relationships'] = $this->Caregiver_Home_model->calculate_topic('axel','Relationships');
+            $data['other'] = $this->Caregiver_Home_model->calculate_topic('axel','Other');
+            
+            $data['page_content'] = 'Caregiver/personal.html';
+            $this->parser->parse('master.php', $data);
+        } else {
             echo "You are not allowed to access this page!!!";
         }
     }
-    
-    function tips(){
+
+    function tips() {
         if ($this->session->userType == "Caregiver") { // if session exists
             $data['show_navbar'] = true;
             $data['page_title'] = 'Tips';
@@ -63,13 +79,13 @@ class CaregiverController extends CI_Controller{
             $data['navbar_content'] = 'Caregiver/caregiverNavbar.html';
             $data['content'] = "Tips to be added.";
             $data['page_content'] = 'Caregiver/template.html';
-            $this->parser->parse('master.php',$data);
-        }else {
+            $this->parser->parse('master.php', $data);
+        } else {
             echo "You are not allowed to access this page!!!";
         }
     }
-    
-    function profile(){
+
+    function profile() {
         if ($this->session->userType == "Caregiver") {
             $data['change_password'] = $this->lang->line('caregiver_change_password');
             $data['old_password'] = $this->lang->line('caregiver_old_password');
@@ -84,40 +100,40 @@ class CaregiverController extends CI_Controller{
             $data['page_content'] = 'Account/profile.html';
             $data['Person_Name'] = $this->session->Name;
             $this->parser->parse('master.php', $data);
-        }
-        else {
+        } else {
             echo "You are not allowed to access this page!!!";
         }
     }
-    
-    function change_language(){
+
+    function change_language() {
         $newlang = $this->input->post('language');
-        if(isset($newlang)){
+        if (isset($newlang)) {
             $this->session->set_userdata('Language', $newlang);
             $this->db->query("UPDATE a16_webapps_2.Caregiver "
-                        . "SET Language ='".$newlang."' "
-                        . "WHERE idCaregiver = " .$this->session->idCaregiver .";");
+                    . "SET Language ='" . $newlang . "' "
+                    . "WHERE idCaregiver = " . $this->session->idCaregiver . ";");
         }
         redirect(base_url() . 'CaregiverController/profile');
     }
-    
-    function change_password(){
+
+    function change_password() {
         $username = $this->Caregiver_Home_model->get_name($this->session->idCaregiver);
         //echo $username;
-        $verif = $this->db->query("SELECT password, Name FROM a16_webapps_2.Caregiver WHERE Name = '" . $username."';")->row();
+        $verif = $this->db->query("SELECT password, Name FROM a16_webapps_2.Caregiver WHERE Name = '" . $username . "';")->row();
         $old = filter_input(INPUT_POST, 'old_password');
         $new = $this->input->post('new_password');
         $conf = $this->input->post('conf_password');
-        if($conf === $new){
+        if ($conf === $new) {
             echo "passwords equal";
-            if(password_verify($old, $verif->password)){
+            if (password_verify($old, $verif->password)) {
                 $password = password_hash($new, PASSWORD_DEFAULT);
                 echo " -> should update";
                 $this->db->query("UPDATE a16_webapps_2.Caregiver "
-                        . "SET Name ='".$username."' , password = '".$password."' "
-                        . "WHERE idCaregiver = " .$this->session->caregiver_id .";");
+                        . "SET Name ='" . $username . "' , password = '" . $password . "' "
+                        . "WHERE idCaregiver = " . $this->session->caregiver_id . ";");
             }
         }
         $this->profile();
     }
+
 }
--- GraceAge/application/models/Caregiver_Home_model.php
+++ GraceAge/application/models/Caregiver_Home_model.php
@@ -11,24 +11,79 @@
  *
  * @author orditech
  */
-class Caregiver_Home_model extends CI_Model{
+class Caregiver_Home_model extends CI_Model {
+
     public function __construct() {
         parent::__construct();
         //$this->load->database();
     }
-    
-    function get_topics(){
+
+    function get_topics() {
         $query = $this->db->distinct()->select('Topic')->get('Question');
         return $query->result();
     }
-    
-    function get_name($id){
+
+    function get_name($id) {
         $query = $this->db->query("SELECT Name FROM a16_webapps_2.Caregiver WHERE idCaregiver = " . $id);
         return $query->row()->Name;
     }
-    
-    function get_patients(){
+
+    function get_patients() {
         $query = $this->db->select('Name')->get('Patient');
         return $query->result();
     }
+
+    function print_score($username) {
+        /* $query = $this->db->query("SELECT idPatient FROM Patient where Name= '?' ", $username); */
+        $query = $this->db->select('idPatient')->where('Name', $username)->get('Patient');
+        $id = $query->result();
+        $id2 = $id[0]->idPatient;
+        $query = $this->db->select('Answer')->where('Patient_idPatient', $id2)->order_by('DateTime', 'DESC')->limit(1)->get('Patient_Answered_Question');
+        return $query->result();
+    }
+
+    function calculate_topic($username, $topic) {
+        $query = $this->db->select('idPatient')->where('Name', $username)->get('Patient');
+        $id = $query->result();
+        $id2 = $id[0]->idPatient;
+
+        $query = $this->db->select('idQuestion')->where('Topic', $topic)->get('Question');
+        $questions = $query->result();
+        $amount = count($questions);
+
+        unset($questionids);
+        $questionids = array();
+
+        for ($x = 0; $x < $amount; $x++) {
+            $questionids[$x] = $questions[$x]->idQuestion;
+        }
+        
+        unset($antwoordenarray);
+        $antwoordenarray = array();
+        
+        foreach($questionids as $idss){
+            $voorwaarde = array('Patient_idPatient' => $id2, 'Question_Number' => $idss);
+            $query = $this->db->select('Answer')->where($voorwaarde)->order_by('DateTime', 'DESC')->limit(1)->get('Patient_Answered_Question');
+            $antwoord = $query->result();
+            $antwoord2 = $antwoord[0]->Answer;
+            array_push($antwoordenarray, $antwoord2);
+        }
+        
+        foreach($antwoordenarray as $kk){
+            
+            echo $kk;
+            
+            
+        }
+        echo $amount;
+        $som = array_sum($antwoordenarray) * 25 / $amount;
+        
+        echo $som;
+        
+        
+        
+        
+        return $som;
+    }
+
 }
