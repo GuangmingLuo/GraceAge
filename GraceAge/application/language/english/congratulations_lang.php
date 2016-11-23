ce9522263976acbcac0d5f38bd160e7cf8e76891
r0448480 <r0448480@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-23 20:36:38 +0000

    finished the layout of the "congratulations-page" added bootstrap-grid layout and styling. Also made changes in navbar elderly

--- GraceAge/application/language/english/congratulations_lang.php
+++ GraceAge/application/language/english/congratulations_lang.php
@@ -4,5 +4,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['congrats_congrats_message'] = 'CONGRATULATIONS!';
 $lang['congrats_your_score_is'] ='YOUR SCORE IS: ';
-$lang['congrats_these_can_be_exchanged'] = 'THESE VAN BE EXCHANGED AT YOUR SCORE OVERVIEW FOR A REWARD';
+$lang['congrats_these_can_be_exchanged'] = 'THESE CAN BE EXCHANGED AT YOUR SCORE OVERVIEW FOR A REWARD';
 $lang['congrats_button_text'] = 'PROCEED TO ANSWER QUESTIONS';
