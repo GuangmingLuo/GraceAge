0f02046505432a56a5279e457fa63af3766ed4e3
r0448480 <r0448480@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-23 20:39:36 +0000

    translated "log in" to "aanmelden" on the login page

--- GraceAge/application/language/dutch/login_lang.php
+++ GraceAge/application/language/dutch/login_lang.php
@@ -1,8 +1,8 @@
 <?php
 // DUTCH //
 defined('BASEPATH') OR exit('No direct script access allowed');
-$lang['BAGDE'] = 'BAGDE';
-$lang['LOG_IN'] = "LOG IN";
+$lang['BAGDE'] = 'BADGE';
+$lang['LOG_IN'] = "AANMELDEN";
 $lang['show_your_badge'] = "Toon uw Badge";
 $lang['no_camera'] = "Geen camera? Gebruik een foto van uw badge in de plaats.";
 $lang['credentials'] = "Vul uw gegevens in.";
