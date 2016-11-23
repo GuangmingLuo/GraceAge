f2a3b74689093f3afc7e07854ecaff7af14d4fde
r0448480 <r0448480@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-23 15:19:24 +0000

    styling elderly homepage: changes in index.html, elderlyHome.css english/elderly_lang

--- GraceAge/application/language/english/elderly_lang.php
+++ GraceAge/application/language/english/elderly_lang.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['hello'] = 'Hello';
 $lang['elderly_settings_button']='WHO AM I?';
 $lang['elderly_stop_button']='STOP APPLICATION';
-$lang['elderly_tips_button']='PUSH HERE TO VIEW TIPS AND WEEKLY GOALS';
+$lang['elderly_tips_button']='PUSH HERE TO VIEW TIPS AND GOALS';
 $lang['elderly_score_button']='PUSH HERE TO VIEW YOUR SCORE';
 $lang['elderly_questionnaire_button']='PUSH HERE TO START ANSWERING QUESTIONS';
