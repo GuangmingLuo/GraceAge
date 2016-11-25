bafc77f5d426f1b4cbfb9ff1ac23f57d80a867fe
r0448480 <r0448480@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-11-25 21:43:56 +0000

    restyling login page

--- GraceAge/application/language/dutch/caregiver_lang.php
+++ GraceAge/application/language/dutch/caregiver_lang.php
@@ -2,10 +2,10 @@
 // DUTCH //
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['hello'] = 'Hallo';
-$lang['caregiver_change_password'] = 'Verander paswoord';
-$lang['caregiver_old_password'] = 'Geef uw oud paswoord in';
-$lang['caregiver_new_password'] = 'Geef uw nieuw paswoord in';
-$lang['caregiver_conf_password'] = 'Bevestig uw nieuw paswoord';
+$lang['caregiver_change_password'] = 'Verander wachtwoord';
+$lang['caregiver_old_password'] = 'Geef uw oud wachtwoord in';
+$lang['caregiver_new_password'] = 'Geef uw nieuw wachtwoord in';
+$lang['caregiver_conf_password'] = 'Bevestig uw nieuw wachtwoord';
 $lang['caregiver_change_lang'] = 'Wijzig uw taal';
 $lang['caregiver_apply'] = 'Pas toe';
 $lang['caregiver_log_out'] = 'Uitloggen';
