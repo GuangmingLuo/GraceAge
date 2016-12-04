b20e0fb387febc1c1bc1e8d171eade454dcab6c6
r0304369 <r0304369@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-12-04 21:26:03 +0000

    profile saves language or pass or both

--- GraceAge/assets/js/profilescript.js
+++ GraceAge/assets/js/profilescript.js
@@ -9,22 +9,26 @@ function postForm() {
 
     var select = document.getElementById("language");
     var lang = select.options[select.selectedIndex].value;
+    $.post("change_language", {language: lang}, null);
     var old = document.getElementById("old").value;
-    
+
     var newpass = $("#new").val();
     var conf = $("#conf").val();
-    
-    if(old == false || newpass == false || conf ==false){
-        alert("fields empty");
-    }
-    else if(newpass=== conf){
-        
-        $.post("change_language", {language: lang}, null);
-        $.post("change_password",{old_password: old, new_password: newpass, conf_password: conf}, null);
-        alert("changes saved")
-    }
-    else alert("the new pasword does not match");
-    
 
-};
+    if (!(old == false || newpass == false || conf == false)) {
+        if (newpass === conf) {
+
+
+            $.post("change_password", {old_password: old, new_password: newpass, conf_password: conf});
+
+        } else
+            alert("the new pasword does not match");
+    }
+
+    alert("changes saved")
+
+
+}
+;
+
 
