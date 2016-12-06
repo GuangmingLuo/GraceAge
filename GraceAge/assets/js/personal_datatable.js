03f3939e43ac8d9862f202fc021c8d63e6e6fd7f
r0365476 <r0365476@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-12-06 15:25:56 +0000

    Personal page now has a datatable containing the names of the patients with some hardcoded data. Clicking on a name can either show an empty dialog box or show the scores in Christophe's table.

---
+++ GraceAge/assets/js/personal_datatable.js
@@ -0,0 +1,24 @@
+/*
+ * Automatically sorts on name
+ * Default shows first 10 entries
+ * Allows to search on name.
+ */
+$(document).ready(function(){
+    //Makes the datatable
+    var table = $('#personal-datatable').DataTable();
+    
+    //Creates the dialog pop-up.
+    $("#display-scores").dialog({
+            autoOpen: false,
+            buttons: {
+                OK: function() {$(this).dialog("close");}
+            }
+    });
+    
+    //When clicked on a table row, this function is called, which opens the dialog screen.
+    $('#personal-datatable tbody').on('click', 'tr', function(){
+        //var data = table.row(this).data();
+        //window.location.href = "personal?username="+data[0];  //This displays the scores in Christophe's table.
+        $('#display-scores').dialog("open");    //Opens a dialog screen. This should contain the data.
+    });
+});
