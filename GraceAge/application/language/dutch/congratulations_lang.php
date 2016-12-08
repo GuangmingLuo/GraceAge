50cfab60993410e1d8fe9b61d19ed3a40acebced
r0303140 <r0303140@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-12-08 16:07:14 +0000

    fixed styling for congratiulations page

--- GraceAge/application/language/dutch/congratulations_lang.php
+++ GraceAge/application/language/dutch/congratulations_lang.php
@@ -2,7 +2,7 @@
 
 // DUTCH //
 defined('BASEPATH') OR exit('No direct script access allowed');
-$lang['congrats_congrats_message'] = 'PROFICIAT! U HEBT EEN VOLLEDIGE ENQUETE INGEVULD';
-$lang['congrats_your_score_is'] = 'UW SCORE IS: ';
-$lang['congrats_these_can_be_exchanged'] = 'DEZE KUNNEN WORDEN INGEWISSELD VOOR EEN BELONING IN HET SCORE OVERZICHT';
+$lang['congrats_congrats_message'] = 'PROFICIAT! Je hebt een volledige enquète ingevuld';
+$lang['congrats_your_score_is'] = 'JE HEBT NU EEN SCORE VAN: ';
+$lang['congrats_these_can_be_exchanged'] = 'Deze kunnen in het score overzicht ingewisseld worden voor een beloning';
 $lang['congrats_button_text'] = 'GA VERDER MET DE VRAGENLIJST';
