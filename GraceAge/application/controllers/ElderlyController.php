ec698b3b5700377e3d0f68a00226ffafdce81908
r0304369 <r0304369@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-12-08 13:32:33 +0000

    placed all the $data[ ..] = ... into separate functions load...Data() for the ElderlyController

--- GraceAge/application/controllers/ElderlyController.php
+++ GraceAge/application/controllers/ElderlyController.php
@@ -45,43 +45,51 @@ class ElderlyController extends CI_Controller {
             return false;
         }
     }
-
+    
+    private function  loadIndexData(){
+        $data = $this->loadComonData();
+        $data['show_navbar'] = false;
+        $data['settings_button'] = $this->lang->line('elderly_settings_button');
+        $data['stop_button'] = $this->lang->line('elderly_stop_button');
+        $data['questionnaire_button'] = $this->lang->line('elderly_questionnaire_button');
+        $data['tips_button'] = $this->lang->line('elderly_tips_button');
+        $data['score_button'] = $this->lang->line('elderly_score_button');
+        
+        $data['page_title'] = 'Elderly Home';
+        $data['header1'] = 'Welcome to Elderly Home';
+        $data['menu_items'] = $this->Menu_model->get_menuitems('Home');
+        $data['content'] = "This is the home page! welcome " . $this->session->Name;
+        $data['page_content'] = 'Elderly/index.html';
+        return $data;
+    }
     function index() {
         if (!$this->is_logged_in()) return;  // if session exists
-            $data['show_navbar'] = false;
-            $data['settings_button'] = $this->lang->line('elderly_settings_button');
-            $data['stop_button'] = $this->lang->line('elderly_stop_button');
-            $data['questionnaire_button'] = $this->lang->line('elderly_questionnaire_button');
-            $data['tips_button'] = $this->lang->line('elderly_tips_button');
-            $data['score_button'] = $this->lang->line('elderly_score_button');
-            $data['navbar_content'] = 'Elderly/elderlyNavbar.html';
-            $data['page_title'] = 'Elderly Home';
-            $data['header1'] = 'Welcome to Elderly Home';
-            $data['menu_items'] = $this->Menu_model->get_menuitems('Home');
-            $data['content'] = "This is the home page! welcome " . $this->session->Name;
-            $data['page_content'] = 'Elderly/index.html';
+            $data =$this->loadIndexData();
             $this->parser->parse('master.php', $data);
         
     }
 
     /*     * ************* All Questionnaire page functions *********************** */
-
+    private function loadQuestionnaireData() {
+        $data = $this->loadComonData();
+        $data['questionNumber'] = ($this->session->question_id);                // set initial questionNumber for progressbar
+        $data['pbQuestionText'] = lang('question_text');                        // set label of progressbar
+        $data['initial_pbWidth'] = (($this->session->question_id) / 52) * 100;      // set initial width of progressbar
+        $data['page_title'] = 'Questionnaire';
+        $data['header1'] = 'Questionnaire';
+        $data['menu_items'] = $this->Menu_model->get_menuitems('Questionnaire');
+        $data['answers'] = $this->Question_model->get_answerbuttons();
+        $data['navigationbuttons'] = $this->Question_model->get_navigationbuttons();
+        $data['questions'] = $this->Question_model->get_question($this->session->question_id, $this->session->Language);
+        $data['page_content'] = 'Elderly/questionnaire.php';
+        return $data;
+    }
     function questionnaire() {
         if (!$this->is_logged_in()) return;  // if session exists
             //Go fetch necessary data from database to setup the correct question.
             $this->Question_model->get_initial_state();            
-            $data['show_navbar'] = true;
-            $data['questionNumber'] = ($this->session->question_id);                // set initial questionNumber for progressbar
-            $data['pbQuestionText'] = lang('question_text');                        // set label of progressbar
-            $data['initial_pbWidth'] = (($this->session->question_id)/52)*100;      // set initial width of progressbar
-            $data['navbar_content'] = 'Elderly/elderlyNavbar.html';
-            $data['page_title'] = 'Questionnaire';
-            $data['header1'] = 'Questionnaire';
-            $data['menu_items'] = $this->Menu_model->get_menuitems('Questionnaire');
-            $data['answers'] = $this->Question_model->get_answerbuttons();
-            $data['navigationbuttons'] = $this->Question_model->get_navigationbuttons();
-            $data['questions'] = $this->Question_model->get_question($this->session->question_id, $this->session->Language);
-            $data['page_content'] = 'Elderly/questionnaire.php';
+            $data = $this->loadQuestionnaireData();
+
             $this->parser->parse('master.php', $data);
        
     }
@@ -103,28 +111,50 @@ class ElderlyController extends CI_Controller {
 
     /*     * **********************End of Questionnaire functions*************************** */
 
+    private function loadTipsData(){
+        $data = $this->loadComonData();
+        $data['page_title'] = $this->lang->line('tips');
+        $data['header'] = $this->lang->line('overview_of_tips');
+        $data['menu_items'] = $this->Menu_model->get_menuitems('Tips');
+        $data['navigationbuttons'] = $this->Tip_model->get_navigationbuttons();
+        $topics = $this->Caregiver_Home_model->get_topics_with_lowest_scores('4');
+        $data['tip_1'] = $this->Tip_model->get_tip($topics[0]);
+        $data['tip_2'] = $this->Tip_model->get_tip($topics[1]);
+        $data['tip_3'] = $this->Tip_model->get_tip($topics[2]);
+        $data['tip_4'] = $this->Tip_model->get_tip($topics[3]);
+        $data['page_content'] = 'Elderly/tips.html';
+        return $data;
+    }
+    
     function tips() {
         if (!$this->is_logged_in()) return; // if session exists
-            $data['show_navbar'] = true;
-            $data['navbar_content'] = 'Elderly/elderlyNavbar.html';
-            $data['page_title'] = $this->lang->line('tips');;
-            $data['header'] = $this->lang->line('overview_of_tips');
-            $data['menu_items'] = $this->Menu_model->get_menuitems('Tips');
-            $data['navigationbuttons'] = $this->Tip_model->get_navigationbuttons();
-            $topics = $this->Caregiver_Home_model->get_topics_with_lowest_scores('4');            
-            $data['tip_1'] = $this->Tip_model->get_tip($topics[0]);
-            $data['tip_2'] = $this->Tip_model->get_tip($topics[1]);
-            $data['tip_3'] = $this->Tip_model->get_tip($topics[2]);
-            $data['tip_4'] = $this->Tip_model->get_tip($topics[3]);
-            $data['page_content'] = 'Elderly/tips.html';
+            
+            $data = $this->loadTipsData();
+
             $this->parser->parse('master.php', $data);
         
     }
     
-    
+    private function loadScoreData(){
+        $data = $this->loadComonData();
+            $data['score_text'] = lang('score_text');
+            $data['score'] = $this->Question_model->getPatientScore($this->session->idPatient);
+            $data['exchange_score'] = lang('exchange_score');
+            $data['buy_reward'] = lang('buy_reward');
+            $data['reward_text'] = lang('reward_text');
+            $data['rewards_bought_text'] = lang('rewards_bought_text');
+            $data['page_title'] = 'Score';
+            $data['header1'] = 'Your score';
+            $data['rewards'] = $this->Question_model->getRewards($this->session->Language);
+            $data['rewards_bought'] = $this->Question_model->getRewardsBought($this->session->idPatient);
+            $data['menu_items'] = $this->Menu_model->get_menuitems('Score');
+            $data['page_content'] = 'Elderly/score.html';
+            return $data;
+        
+    }
     function score() {
         if (!$this->is_logged_in()) return;
-
+            $data = $this->loadScoreData();
             $rewardBought = "false";
             $data['reward_message'] = lang('buy_the_reward');
             
@@ -138,22 +168,7 @@ class ElderlyController extends CI_Controller {
                 }      
             }
             
-            $data['show_navbar'] = true;
-            $data['navbar_content'] = 'Elderly/elderlyNavbar.html';
-            $data['score_text'] = lang('score_text');
-            $data['score'] = $this->Question_model->getPatientScore($this->session->idPatient);
-            $data['exchange_score'] = lang('exchange_score');
-            $data['buy_reward'] = lang('buy_reward');
-            $data['reward_text'] = lang('reward_text');
-            $data['rewards_bought_text'] = lang('rewards_bought_text');
-            $data['page_title'] = 'Score';
-            $data['header1'] = 'Your score';
-
-            $data['rewards'] = $this->Question_model->getRewards($this->session->Language);
-
-            $data['rewards_bought'] = $this->Question_model->getRewardsBought($this->session->idPatient);
-            $data['menu_items'] = $this->Menu_model->get_menuitems('Score');
-            $data['page_content'] = 'Elderly/score.html';
+            
             $this->parser->parse('master.php', $data);
       
     }
@@ -165,12 +180,9 @@ class ElderlyController extends CI_Controller {
         $rewardBought = ($this->Question_model->buyReward($reward, $idPatient)) ? 'true' : 'false';
         redirect(base_url() . 'ElderlyController/score?rewardBought=' . $rewardBought);
     }
-
-    function congratulations() {
-        if (!$this->is_logged_in()) return;
-        $this->lang->load('congratulations', $this->session->Language);
-        $data['show_navbar'] = true;
-        $data['navbar_content'] = 'Elderly/elderlyNavbar.html';
+    
+    private function loadCongratulationsData(){
+        $data = $this->loadComonData();
         $data['page_title'] = 'Congratulations';
         $data['menu_items'] = $this->Menu_model->get_menuitems('Questionnaire');
         $data['content'] = "congratulations!!!";
@@ -180,30 +192,41 @@ class ElderlyController extends CI_Controller {
         $data['these_can_be_exchanged'] = $this->lang->line('congrats_these_can_be_exchanged');
         $data['button_text'] = $this->lang->line('congrats_button_text');
         $data['score'] = $this->Question_model->getPatientScore($this->session->idPatient);
+        return $data;
+    }
+
+    function congratulations() {
+        if (!$this->is_logged_in()) return;
+        $this->lang->load('congratulations', $this->session->Language);
+       
         //$this->Question_model->updatePatientScore($this->session->idPatient, 1);
         $this->parser->parse('master.php', $data);
     }
 
+    private function loadProfileData() {
+        $data = $this->loadComonData();
+        $data['menu_items'] = $this->Menu_model->get_menuitems('Questionnaire');
+        $data['log_out'] = $this->lang->line('caregiver_log_out');
+        $data['logout'] = $this->lang->line('caregiver_logout');
+        $data['new_placeholder'] = $this->lang->line('caregiver_new_placeholder');
+        $data['old_placeholder'] = $this->lang->line('caregiver_old_placeholder');
+        $data['conf_placeholder'] = $this->lang->line('caregiver_conf_placeholder');
+        $data['change_password'] = $this->lang->line('caregiver_change_password');
+        $data['old_password'] = $this->lang->line('caregiver_old_password');
+        $data['new_password'] = $this->lang->line('caregiver_new_password');
+        $data['conf_password'] = $this->lang->line('caregiver_conf_password');
+        $data['change_lang'] = $this->lang->line('caregiver_change_lang');
+        $data['Apply'] = $this->lang->line('caregiver_apply');
+        $data['profile'] = $this->lang->line('caregiver_menu_profile');
+        $data['page_title'] = 'Edit Profile';
+        $data['page_content'] = 'Account/elderly_profile.html';
+        $data['Person_Name'] = $this->session->Name;
+        return $data;
+    }
+
     function profile(){
         if (!$this->is_logged_in()) return;
-            $data['show_navbar'] = true;
-            $data['navbar_content'] = 'Elderly/elderlyNavbar.html';
-            $data['menu_items'] = $this->Menu_model->get_menuitems('Questionnaire');
-            $data['log_out'] = $this->lang->line('caregiver_log_out');            
-            $data['logout'] = $this->lang->line('caregiver_logout');
-            $data['new_placeholder'] = $this->lang->line('caregiver_new_placeholder');
-            $data['old_placeholder'] = $this->lang->line('caregiver_old_placeholder');
-            $data['conf_placeholder'] = $this->lang->line('caregiver_conf_placeholder');
-            $data['change_password'] = $this->lang->line('caregiver_change_password');
-            $data['old_password'] = $this->lang->line('caregiver_old_password');
-            $data['new_password'] = $this->lang->line('caregiver_new_password');
-            $data['conf_password'] = $this->lang->line('caregiver_conf_password');
-            $data['change_lang'] = $this->lang->line('caregiver_change_lang');
-            $data['Apply'] = $this->lang->line('caregiver_apply');
-            $data['profile'] = $this->lang->line('caregiver_menu_profile');
-            $data['page_title'] = 'Edit Profile';
-            $data['page_content'] = 'Account/elderly_profile.html';
-            $data['Person_Name'] = $this->session->Name;
+            $data = $this->loadProfileData();
             $this->parser->parse('master.php', $data);
         
     }
