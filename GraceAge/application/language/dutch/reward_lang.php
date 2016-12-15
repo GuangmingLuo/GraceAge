af2f06dd91f2a0f299be95593a504d3334def10e
r0448480 <r0448480@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-12-15 17:47:35 +0000

    Styling caregiver: bootstrap reward-page, title and frame

--- GraceAge/application/language/dutch/reward_lang.php
+++ GraceAge/application/language/dutch/reward_lang.php
@@ -2,7 +2,7 @@
 // DUTCH //
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['write_new_reward'] = 'Schrijven een nieuwe beloning';
-$lang['add_new_reward'] = 'Voeg een nieuwe beloning';
+$lang['add_new_reward'] = 'Voeg toe';
 $lang['price'] = 'Prijs';
 $lang['Reward'] = 'Beloning: ';
 $lang['Price'] = ' Prijs: ';
--- GraceAge/application/language/english/reward_lang.php
+++ GraceAge/application/language/english/reward_lang.php
@@ -2,7 +2,7 @@
 // ENGLISH //
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['write_new_reward'] = 'Write a new reward';
-$lang['add_new_reward'] = 'Add a new reward';
+$lang['add_new_reward'] = 'Add';
 $lang['price'] = 'Price';
 $lang['Reward'] = 'Reward: ';
 $lang['Price'] = ' Price: ';
