0ba5de34908131e27123a822a8784b2155090d62
r0364529 <r0364529@3c785f37-70d7-46d7-8ce8-d33f9020bd7f>
2016-12-16 10:50:11 +0000

    Fixed fade out message (forgot #) (Sorry Daniela for not seeing it first)

--- GraceAge/assets/js/caregiver_messages.js
+++ GraceAge/assets/js/caregiver_messages.js
@@ -2,9 +2,9 @@ function send_msg() {
     var message = document.getElementById("btn-input").value;
     $.post("send_message", {message : message});
     document.getElementById("updatebox").innerHTML = "Send!";
-    $('updatebox').show();
+    //$('updatebox').show();
     setTimeout(function () {
-        $('updatebox').fadeOut();
-    }, 1000); // <-- time in milliseconds
+        $('#updatebox').fadeOut();
+    }, 1500); // <-- time in milliseconds
 }
 ;
